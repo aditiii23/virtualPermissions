227a745f999433b7f9dd61a83e6add063a75f8eb
Aditi Maurya <imt_2019006@iiitm.ac.in>
2022-12-13 17:03:33 +0530

    Add route to view user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const { loginUser, registerUser } = require("../controllers/userController")
+const {
+  loginUser,
+  registerUser,
+  viewUser,
+} = require("../controllers/userController")
 
 router.post("/registerUser", registerUser)
 router.post("/login", loginUser)
+router.get("/view", viewUser)
 
 module.exports = router
