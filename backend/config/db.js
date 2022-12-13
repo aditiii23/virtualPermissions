ab081da509c93633f55a553f16a4f3a79006018a
Aditi Maurya <imt_2019006@iiitm.ac.in>
2022-12-13 20:09:27 +0530

    Update functions to connect and disconnect the database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,31 @@
 const mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+var databaseConnection
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    databaseConnection = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     })
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(
+      `Database connected ::: ${databaseConnection.connection.host}`.blue
+        .underline
+    )
   } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold)
+    console.error(`Error ::: ${error.message}`)
     process.exit(1)
   }
 }
 
-module.exports = connectDB
+const disconnectDB = async () => {
+  try {
+    await databaseConnection.disconnect()
+    console.log("Database disconnected")
+  } catch (error) {
+    console.error(`Error ::: ${error.message}`.red.bold)
+    process.exit(1)
+  }
+}
+
+module.exports = { connectDB, disconnectDB }
