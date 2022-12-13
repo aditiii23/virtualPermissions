9004a121dec1efc646ea11a2e0531de5302663f8
Aditi Maurya <imt_2019006@iiitm.ac.in>
2022-12-13 20:11:52 +0530

    Add route to generate pass

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const router = express.Router()
+const { protect } = require("../middleware/authMiddleware")
 const {
   loginUser,
   registerUser,
   viewUser,
 } = require("../controllers/userController")
 
+const { generatePass } = require("../controllers/passController")
+
 router.post("/registerUser", registerUser)
 router.post("/login", loginUser)
 router.get("/view", viewUser)
+router.post("/generatePass", protect, generatePass)
 
 module.exports = router
