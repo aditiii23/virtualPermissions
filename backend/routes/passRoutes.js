77199a1cdefa646c7c5caf5a0cef261fedad2f66
Aditi Maurya <imt_2019006@iiitm.ac.in>
2022-12-16 18:00:04 +0530

    Add route to verify pass

--- backend/routes/passRoutes.js
+++ backend/routes/passRoutes.js
@@ -2,10 +2,16 @@ const express = require("express")
 const router = express.Router()
 const { protect } = require("../middleware/authMiddleware")
 
-const { generatePass, viewPasses } = require("../controllers/passController")
+const {
+  generatePass,
+  viewPasses,
+  verifyPass,
+} = require("../controllers/passController")
 
 router.post("/generatePass", protect, generatePass)
 
 router.get("/viewPasses", protect, viewPasses)
+
+router.put("/verifyPass/:_id", protect, verifyPass)
 
 module.exports = router
