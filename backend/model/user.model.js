dc263a48b00f9e8ddb4d4f5aa7913cf0fd84d575
Aditi Maurya <imt_2019006@iiitm.ac.in>
2022-12-16 15:42:42 +0530

    Add role

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -23,6 +23,10 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
