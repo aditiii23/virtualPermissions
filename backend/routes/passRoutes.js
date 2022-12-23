84732dbf55279296fe47f720a5c1026322f291bd
Aditi Maurya <imt_2019006@iiitm.ac.in>
2022-12-23 11:44:59 +0530

    Add feature to verify pass in backend

--- backend/routes/passRoutes.js
+++ backend/routes/passRoutes.js
@@ -2,9 +2,14 @@ const express = require("express")
 const router = express.Router()
 const { protect, authorize } = require("../middleware/authMiddleware")
 
-const { generatePass, viewPasses } = require("../controllers/passController")
+const {
+  generatePass,
+  viewPasses,
+  verifyPass,
+} = require("../controllers/passController")
 
 router.post("/generatePass", protect, authorize("admin"), generatePass)
 router.get("/viewPasses", protect, authorize("admin"), viewPasses)
+router.put("/verifyPass/:_id", protect, authorize("guard"), verifyPass)
 
 module.exports = router
