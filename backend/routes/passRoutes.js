a3f9f4510ebfb6bfd3bc69b28da00c693fbb050d
Aditi Maurya <imt_2019006@iiitm.ac.in>
2022-12-23 11:43:53 +0530

    Add feature to view passes in backend

--- backend/routes/passRoutes.js
+++ backend/routes/passRoutes.js
@@ -2,8 +2,9 @@ const express = require("express")
 const router = express.Router()
 const { protect, authorize } = require("../middleware/authMiddleware")
 
-const { generatePass } = require("../controllers/passController")
+const { generatePass, viewPasses } = require("../controllers/passController")
 
 router.post("/generatePass", protect, authorize("admin"), generatePass)
+router.get("/viewPasses", protect, authorize("admin"), viewPasses)
 
 module.exports = router
