da2aaf3a70820e6372bfd4240718726587867f75
Aditi Maurya <56069235+aditiii23@users.noreply.github.com>
2023-01-02 19:40:06 +0530

    Generate pass, verify pass and view passes (#4)
    * Add model for pass generation in backend
    
    * Add feature to generate pass in backend
    
    * Add feature to view passes in backend
    
    * Add feature to verify pass in backend
    
    * Remove console logs in passController in backend
    
    * Remove protect middleware from passRoutes
    
    * Add fixes for #4
    
    * Add fixes for #4
    
    * Update changes from #4
    
    * Update changes from #4
    
    * Remove connectDB() from passController and add error log in errorMiddleware
    
    * Remove checkInStatus
    
    * Remove import of connectDB()
    
    * Remove query and initialize user using authMiddleware data
    
    * Remove extra blank lines from authMiddleware
    
    * Return only specific fields at login
    
    * Remove line
    
    * Remove line to assign user from query
    
    * Remove length check for passes in viewPasses controller
    
    * Remove check for passes in viewPasses controller

---
+++ backend/routes/passRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+const { authorize } = require("../middleware/authMiddleware")
+
+const {
+  generatePass,
+  viewPasses,
+  verifyPass,
+} = require("../controllers/passController")
+
+router.post("/generatePass", authorize("admin"), generatePass)
+router.get("/viewPasses", authorize("admin"), viewPasses)
+router.put("/verifyPass/:_id", authorize("guard"), verifyPass)
+
+module.exports = router
