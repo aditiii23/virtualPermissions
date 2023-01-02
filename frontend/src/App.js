531d01b78f3871e504ae10bc794e04ae55c7ac23
Aditi Maurya <imt_2019006@iiitm.ac.in>
2023-01-02 18:40:40 +0530

    Add profile component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Login from "./components/Login/Login"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Signup from "./components/Signup/Signup"
+import Profile from "./components/Profile/Profile"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
           <Route path="/register" element={<Signup />}></Route>
         </Routes>
       </Router>
---
+++ frontend/src/components/Profile/Profile.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react"
+import basestyle from "../Base.module.css"
+import GeneratePass from "../GeneratePass/GeneratePass"
+import ViewPasses from "../ViewPasses/ViewPasses"
+
+const Profile = () => {
+  const [click1, setClick1] = useState(false)
+  const [click2, setClick2] = useState(false)
+  // if (click1 == true) {
+  return (
+    <div>
+      {click1 == false ? (
+        <button
+          className={basestyle.button_common}
+          onClick={() => setClick1(true)}
+        >
+          Generate Pass
+        </button>
+      ) : null}
+      {click1 ? <GeneratePass /> : null}
+      {click2 == false ? (
+        <button
+          className={basestyle.button_common}
+          onClick={() => setClick2(true)}
+        >
+          View Passes
+        </button>
+      ) : null}
+      {click2 ? <ViewPasses /> : null}
+    </div>
+  )
+  // } else if (click2 == true && click1==false) {
+  // return <div></div>
+  // }
+}
+export default Profile
