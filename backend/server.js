7af5580cf82ed8e9e343745834756d63788939a7
Aditi Maurya <imt_2019006@iiitm.ac.in>
2023-03-10 21:06:27 +0530

    Update server.js for cors

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,18 @@ connectDB()
 
 const app = express()
 app.use(cors())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, DELETE, PUT, HEAD"
+  )
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+  next()
+})
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
