0b72aa24ea2254e7cfb97e237a6def77452524be
Aditi Maurya <shally.aditi23@gmail.com>
2023-05-11 16:56:22 +0530

    Update parameters for authorize function

--- backend/routes/passRoutes.js
+++ backend/routes/passRoutes.js
@@ -6,12 +6,12 @@ const {
   generatePass,
   viewPasses,
   verifyPass,
-  viewUnverifiedPasses
+  viewUnverifiedPasses,
 } = require("../controllers/passController")
 
-router.post("/generatePass", authorize("admin"), generatePass)
-router.get("/viewPasses", authorize("admin"), viewPasses)
-router.put("/verifyPass/:_id", authorize("guard"), verifyPass)
-router.get("/viewUnverifiedPass", authorize("guard"), viewUnverifiedPasses)
+router.post("/generatePass", authorize(["admin"]), generatePass)
+router.get("/viewPasses", authorize(["admin"]), viewPasses)
+router.put("/verifyPass/:_id", authorize(["guard"]), verifyPass)
+router.get("/viewUnverifiedPass", authorize(["guard"]), viewUnverifiedPasses)
 
 module.exports = router
