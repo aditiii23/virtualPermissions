9734cf3dd4149666d99b479212f708e249aec76d
Aditi Maurya <shally.aditi23@gmail.com>
2023-05-16 11:53:03 +0530

    Add check for missing token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,25 +5,26 @@ const { ErrorHandler } = require("./errorMiddleware")
 const authorize = (roles) => {
   return async (req, res, next) => {
     if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith("Bearer")
     ) {
-      try {
-        let token
-        token = req.headers.authorization.split(" ")[1]
-        if (!token) {
-          throw new ErrorHandler(400, "Not authorized, no token")
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = await User.findById(decoded.id)
-        const user = req.user
-        if (!roles.includes(user.role)) {
-          throw new ErrorHandler(403, "You are not authorized to access this")
-        }
-        next()
-      } catch (error) {
-        next(error)
+      throw new ErrorHandler(400, "Token missing!")
+    }
+    try {
+      let token
+      token = req.headers.authorization.split(" ")[1]
+      if (!token) {
+        throw new ErrorHandler(400, "Not authorized, no token")
       }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      req.user = await User.findById(decoded.id)
+      const user = req.user
+      if (!roles.includes(user.role)) {
+        throw new ErrorHandler(403, "You are not authorized to access this")
+      }
+      next()
+    } catch (error) {
+      next(error)
     }
   }
 }
