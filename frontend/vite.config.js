3a749fbf10ce6b3fd6d4e7b6142ca50f51d61852
Aditi Maurya <shally.aditi23@gmail.com>
2023-05-22 23:12:38 +0530

    Enable PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig(() => {
@@ -6,6 +7,14 @@ export default defineConfig(() => {
     build: {
       outDir: "dist",
     },
-    plugins: [react()],
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+      }),
+      react(),
+    ],
   }
 })
